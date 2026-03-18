fdcf6049dbf19d7525837dde7f95d5da3f304437
Hasanga-Anawarathna <hasangaanawarathna@gmail.com>
2026-03-18 12:07:28 +0530

    Refactor app component to include RouterLink and RouterLinkActive imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('frontend');
-}
+export class App {}
