5e4882cdac64abc69d00f66ab85fd69c3f95709d
Hasanga-Anawarathna <hasangaanawarathna@gmail.com>
2026-03-20 14:09:27 +0530

    Refactor app.routes.ts to reorganize route imports and enhance route definitions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 
-import { DashboardComponent } from './components/dashboard/dashboard';
-import { TransactionsComponent } from './components/transactions/transactions';
+import { Budget } from './components/budget/budget';
 import { CategoriesComponent } from './components/categories/categories';
+import { DashboardComponent } from './components/dashboard/dashboard';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { TransactionsComponent } from './components/transactions/transactions';
 
 export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'transactions', component: TransactionsComponent },
   { path: 'categories', component: CategoriesComponent },
+  { path: 'budget', component: Budget },
   { path: '**', redirectTo: 'dashboard' },
 ];
