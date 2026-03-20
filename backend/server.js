eaaa150173bd1acc115cabdfa12803c91f4c5d81
Hasanga-Anawarathna <hasangaanawarathna@gmail.com>
2026-03-20 15:00:06 +0530

    Add initial server setup with Express and middleware configuration

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/categories", require("./routes/categoryRoutes"));
+app.use("/api/transactions", require("./routes/transactionRoutes"));
+app.use("/api/dashboard", require("./routes/dashboardRoutes"));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
