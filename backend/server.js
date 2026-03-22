0958f4171fd85dbe60b58a2a9883a0d83f5d681f
Hasanga-Anawarathna <hasangaanawarathna@gmail.com>
2026-03-22 16:13:41 +0530

    Refactor server.js for improved readability and consistency in comments

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
-// Middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/categories", require("./routes/categoryRoutes"));
-app.use("/api/transactions", require("./routes/transactionRoutes"));
-app.use("/api/dashboard", require("./routes/dashboardRoutes"));
-app.use("/api/budget", require("./routes/budgetRoutes"));
-
-// Test route
+// test route
 app.get("/", (req, res) => {
-  res.send("Backend is running...");
+  res.send("Money Tracker Backend Running...");
 });
 
-// Port
 const PORT = process.env.PORT || 5000;
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
